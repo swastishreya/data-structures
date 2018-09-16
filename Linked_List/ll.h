2c43f405e13ad60b6e5b5e2cb68c60d1d26af480
sonasingh46 <sonasingh46@gmail.com>
2018-09-17 00:29:59 +0530

    Linked list initial code

---
+++ Linked_List/ll.h
@@ -0,0 +1,13 @@
+#ifndef _LL_H_
+#define _LL_H_
+// Create a structure that will define a node in linked list.
+typedef struct nodeLL{
+    int data;
+    struct nodeLL* link;
+} nodeLL;
+// Function to create a node for Linked List from heap
+nodeLL* getNodeLL(int data);
+nodeLL* insertNodeAtEndLL(nodeLL* root, int data);
+nodeLL* insertNodeAtBegLL(nodeLL* head, int data);
+void displayLL(nodeLL* head);
+#endif
